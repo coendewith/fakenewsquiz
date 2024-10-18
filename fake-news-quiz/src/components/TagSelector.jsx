40fca06aef686229389cce71bd6f96f93bc9ed3b
Coen de With <coendewith@Macbook-Pro-van-Coen.local>
2024-10-18 02:14:41 +0200

    Fix supabase client import

--- fake-news-quiz/src/components/TagSelector.jsx
+++ fake-news-quiz/src/components/TagSelector.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { supabase } from '../services/supabaseclient';
+import { supabase } from '../services/supabaseClient';
 
 function TagSelector({ selectedTag, setSelectedTag, selectedDateRange }) {
   const [tags, setTags] = useState([]);
